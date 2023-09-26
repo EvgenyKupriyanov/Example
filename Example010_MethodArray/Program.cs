384453fa6154aec7d27c720797a8732a77d9c9e5
Evgeny <geny_91@mail.ru>
2023-09-26 14:33:11 +0300

    Example011

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 1, 12, 31, 17, 15, 16, 4, 17, 18 };
+
+int n = array.Length;
+int find = 17;
+
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
